497937032ba090142f585a62765c7124a6214fa8
Patrick Tiefenthaler <patrick.tiefenthaler@students.fh-wels.at>
2022-01-24 11:17:37 +0100

    Hinzufügen Klasse Person und Address

---
+++ Abschlussprojekt/Abschlussprojekt/Address.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiefenthaler_Zehetner_Webshop
+{
+    public class Address //Tiefenthaler
+    {
+        #region private members
+        private string _street;
+        private string _number;
+        private string _countryCode;
+        private string _zipcode;
+        private string _city;
+        #endregion
+
+        #region public properties
+        public string Street
+        {
+            get
+            {
+                return _street;
+            }
+            private set
+            {
+                if (CheckStreetorCity(value))
+                {
+                    _street = value;
+                }
+                else
+                {
+                    new Exception("Invalid Input of Street.");
+                }
+            }
+        }
+        public string Number
+        {
+            get
+            {
+                return _number;
+            }
+            private set
+            {
+                if (CheckNumber(value))
+                {
+                    _number = value;
+                }
+                else
+                {
+                    new Exception("Invalid Input of Number");
+                }
+            }
+        }
+        public string CountryCode
+        {
+            get
+            {
+                return _countryCode;
+            }
+            private set
+            {
+                if (CheckCountryCode(value))
+                {
+                    _countryCode = value;
+                }
+                else
+                {
+                    new Exception("Invalid Input of Countrycode.");
+                }
+            }
+        }
+        public string ZipCode
+        {
+            get
+            {
+                return _zipcode;
+            }
+            private set
+            {
+                if (CheckZipCode(value))
+                {
+                    _zipcode = value;
+                }
+                else
+                {
+                    new Exception("Invalid Input of Zipcode.");
+                }
+            }
+        }
+        public string City
+        {
+            get
+            {
+                return _city;
+            }
+            private set
+            {
+                if (CheckStreetorCity(value))
+                {
+                    _city = value;
+                }
+                else
+                {
+                    new Exception("Invalid input of City.");
+                }
+            }
+        }
+        #endregion
+
+        #region constructor
+        public Address(string street, string number, string countrycode, string zipcode, string city)
+        {
+            Street = street;
+            Number = number;
+            CountryCode = countrycode;
+            ZipCode = zipcode;
+            City = city;
+        }
+        public Address() : this("Musterstraße", "1", "A", "1234", "Musterstadt")
+        {
+
+        }
+        #endregion
+
+        #region public methods
+        public static bool CheckStreetorCity(string inputString)
+        {
+            if (string.IsNullOrWhiteSpace(inputString) || inputString.Length < 2 || !char.IsUpper(inputString[0])) // Inhalt, Länge und Großbuchstaben prüfen
+            {
+                return false;
+            }
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (!(char.IsLetter(inputString[i]) || char.IsWhiteSpace(inputString[i]) || Equals(inputString[i], '-') || 
+                    Equals(inputString[i], '.') || Equals(inputString[i], '/') || Equals(inputString[i], '&'))) // beinhalte Zeichen prüfen
+                {
+                    return false;
+                }
+                if (char.IsWhiteSpace(inputString[i]) || Equals(inputString[i], '-') || Equals(inputString[i], '.') || 
+                    Equals(inputString[i], '/') || Equals(inputString[i], '&')) // keine hintereinanderfolgende Sonderzeichen
+                {
+                    if (Equals(inputString[i], inputString[i+1]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public static bool CheckZipCode(string inputStringNumber)
+        {
+            if (!((inputStringNumber.Length == 4) || (inputStringNumber.Length == 5)) || 
+                   string.IsNullOrWhiteSpace(inputStringNumber) || Equals(inputStringNumber[0], '0')) // Länge der Zahl prüfen
+            {
+                return false;
+            }
+            for (int i = 0; i < inputStringNumber.Length; i++) // alle Zeichen sind eine Ziffer
+            {
+                if (!(char.IsDigit(inputStringNumber[i])))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static bool CheckNumber(string inputStringNumber)
+        {
+            if (!(inputStringNumber.Length <= 5) || Equals(inputStringNumber[0], '0') || string.IsNullOrWhiteSpace(inputStringNumber)) // Längenprüfung, startet nicht mit 0 und letztes Zeichen darf ein kleiner Buchstabe sein
+            {
+                return false;
+            }
+            int correctlength;
+            if (char.IsLetter(inputStringNumber[inputStringNumber.Length - 1]) && char.IsLower(inputStringNumber[inputStringNumber.Length - 1]))
+            {
+                correctlength = inputStringNumber.Length - 1;
+            }
+            else
+            {
+                correctlength = inputStringNumber.Length;
+            }
+            for (int i = 0; i < correctlength; i++) // alle Zeichen sind eine Ziffer
+            {
+                if (!(char.IsDigit(inputStringNumber[i])))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static bool CheckCountryCode(string inputCountrycode)
+        {
+            if (string.IsNullOrWhiteSpace(inputCountrycode) || inputCountrycode.Length > 3)
+            {
+                return false;
+            }
+            for (int i = 0; i < inputCountrycode.Length; i++)
+            {
+                if (!(char.IsLetter(inputCountrycode[i]) || char.IsUpper(inputCountrycode[i])))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public string ToCsvSring(char seperator)
+        {
+            return Street + seperator + Number + seperator + CountryCode + seperator + ZipCode + seperator + City;
+        }
+        #endregion
+
+        #region override methods
+        public override string ToString()
+        {
+            return "Straße: " + _street + "Hausnummer: " + _number + "Länderkennung: " + _countryCode + "PLZ: " + _zipcode + "Gemeinde: " + _city;
+        }
+        #endregion
+    }
+}
---
+++ Abschlussprojekt/Abschlussprojekt/Person.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiefenthaler_Zehetner_Webshop
+{
+    public class Person //Tiefenthaler
+    {
+        #region private members
+        private string _firstname;
+        private string _surname;
+        private DateTime _dayOfBirth;
+        private readonly Address _address;
+        private string _eMailadress;
+        private string _password;
+        private readonly ConsumerBasket _consumerBasket;
+        #endregion
+
+        #region public properties
+        public string FirstName
+        {
+
+            get
+            {
+                return _firstname;
+            }
+            private set
+            {
+                if (CheckStringInput(value))
+                {
+                    _firstname = value;
+                }
+                else
+                {
+                    new Exception("Invalid Input of Name.");
+                }
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            private set
+            {
+                if (CheckStringInput(value))
+                {
+                    _surname = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid Input of Name.");
+                }
+            }
+        }
+        public DateTime DayOfBirth
+        {
+            get
+            {
+                return _dayOfBirth;
+            }
+            private set
+            {
+                if (value <= DateTime.Today && value >= new DateTime(1900, 01, 01))
+                {
+                    _dayOfBirth = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid Input of Day of Birth.");
+                }
+            }
+        }
+        public Address Address
+        {
+            get
+            {
+                return _address;
+            }
+        }
+        public string EMailAdress
+        {
+            get
+            {
+                return _eMailadress;
+            }
+            private set
+            {
+                if (CheckEmailadress(value))
+                {
+                    _eMailadress = value;
+                }
+                else
+                {
+                    new Exception("Invalid Input of emailadress.");
+                }
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            private set
+            {
+                if (CheckPassword(value))
+                {
+                    _password = value;
+                }
+                else
+                {
+                    new Exception("Invalid Input of password");
+                }
+            }
+        }
+        public ConsumerBasket ConsumerBasket
+        {
+            get
+            {
+                return _consumerBasket;
+            }
+        }
+        #endregion
+
+        #region constructor
+        public Person() : this("ErrorName", "ErrorName", new DateTime(1900, 01, 01), new Address(), "xxx@xxx.at", "1234", new ConsumerBasket())
+        {
+
+        }
+        public Person(string firstName, string surName, DateTime dayOfBirth, Address address, string eMailadress, string password, ConsumerBasket consumerbasket)
+        {
+            FirstName = firstName;
+            Surname = surName;
+            DayOfBirth = dayOfBirth;
+            _address = address;
+            EMailAdress = eMailadress;
+            Password = password;
+            _consumerBasket = consumerbasket;
+        }
+        #endregion
+
+        #region public methods
+        public static bool CheckStringInput(string inputString)
+        {
+            if (string.IsNullOrWhiteSpace(inputString) || inputString.Length < 2 || !char.IsUpper(inputString[0])) // Inhalt, Länge und Großbuchstaben prüfen
+            {
+                return false;
+            }
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (!(char.IsLetter(inputString[i]) || Equals(inputString[i], '-') || Equals(inputString[i], '.') || char.IsWhiteSpace(inputString[i]))) // beinhalte Zeichen prüfen
+                {
+                    return false;
+                }
+                if (Equals(inputString[i], '-') || Equals(inputString[i], '.') || char.IsWhiteSpace(inputString[i])) // keine hintereinanderfolgende Sonderzeichen
+                {
+                    if (Equals(inputString[i], inputString[i+1]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public static bool CheckEmailadress(string inputEmailadress)
+        {
+            if (string.IsNullOrWhiteSpace(inputEmailadress) || !inputEmailadress.Contains('@') || !(inputEmailadress.EndsWith(".at") || 
+                inputEmailadress.EndsWith(".de") || inputEmailadress.EndsWith(".com"))) // Endungen und Inhalt prüfen
+            {
+                return false;
+            }
+            for (int i = 0; i < inputEmailadress.Length; i++)
+            {
+                if (!(char.IsLetterOrDigit(inputEmailadress[i]) || Equals(inputEmailadress[i], '-') || 
+                      Equals(inputEmailadress[i], '.') || Equals(inputEmailadress[i], '@'))) // erlaubte Zeichen prüfen
+                {
+                    return false;
+                }
+                int counterdoubleSign = 0;
+                if (Equals(inputEmailadress[i], '@')) // nur ein @-Zeichen
+                {
+                    counterdoubleSign++;
+                }
+                if (counterdoubleSign>1)
+                {
+                    return false;
+                }
+                if (Equals(inputEmailadress[i], '-') || Equals(inputEmailadress[i], '.') || 
+                    Equals(inputEmailadress[i], '@') || char.IsWhiteSpace(inputEmailadress[i])) // keine Sonderzeichen können hintereinander folgen
+                {
+                    if (Equals(inputEmailadress[i], inputEmailadress[i+1]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+        public static bool CheckPassword(string inputPassword) // 4-stelliger PIN-Code
+        {
+            if (string.IsNullOrWhiteSpace(inputPassword) || inputPassword.Length != 4)
+            {
+                return false;
+            }
+            for (int i = 0; i < inputPassword.Length; i++)
+            {
+                if(!char.IsDigit(inputPassword[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static bool GetCheckMethodeForInput(string inputStringformConsole, string variableName)
+        {
+            switch (variableName)
+            {
+                case "Vornamen":
+                    {
+                        return CheckStringInput(inputStringformConsole);
+                    }
+                case "Nachnamen":
+                    {
+                        return CheckStringInput(inputStringformConsole);
+                    }
+                case "Straße":
+                    {
+                        return Address.CheckStreetorCity(inputStringformConsole);
+                    }
+                case "Hausnummer":
+                    {
+                        return Address.CheckNumber(inputStringformConsole);
+                    }
+                case "Länderkennung":
+                    {
+                        return Address.CheckCountryCode(inputStringformConsole);
+                    }
+                case "PLZ":
+                    {
+                        return Address.CheckZipCode(inputStringformConsole);
+                    }
+                case "Ort":
+                    {
+                        return Address.CheckStreetorCity(inputStringformConsole);
+                    }
+                case "E-Mailadresse":
+                    {
+                        return CheckEmailadress(inputStringformConsole);
+                    }
+                case "PIN-Code":
+                    {
+                        return CheckPassword(inputStringformConsole);
+                    }
+                default:
+                    {
+                        return false;
+                    }
+            }
+        }
+        public string ToCsvString(char seperator) // ohne Warenkorb wird nicht benötigt -> beim wieder einlesen der Daten muss ein neuer leerer Warenkorb erstellt werden
+        {
+            return FirstName + seperator + Surname + seperator + DayOfBirth.ToShortDateString() + seperator + Address.ToCsvSring(seperator) + seperator + EMailAdress + seperator + Password;
+        }
+        public static Person ReadPersonFromcsv(string csvline, char seperator)
+        {
+            string[] parts = csvline.Split(seperator);
+            Person readPerson = new Person(parts[0], parts[1], DateTime.Parse(parts[2]), new Address(parts[3], parts[4], parts[5], parts[6], parts[7]), parts[8], parts[9], new ConsumerBasket());
+            return readPerson;
+        }
+        #endregion
+
+        #region override mehtods
+        public override string ToString()
+        {
+            return "Vorname: " + _firstname + "Familienname: " + _surname + "Geburtsdatum: " + _dayOfBirth + Address.ToString() + "E-Mailadresse: " + _eMailadress + "Passwort: " + _password;
+        }
+        #endregion
+    }
+}
