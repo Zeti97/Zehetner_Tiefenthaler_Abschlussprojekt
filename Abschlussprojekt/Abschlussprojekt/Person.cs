39068b130570c45c7d2ab4f1ded5c70d7d2a5a4b
Patrick Tiefenthaler <patrick.tiefenthaler@students.fh-wels.at>
2022-01-24 11:54:41 +0100

    Namespace geändert

--- Abschlussprojekt/Abschlussprojekt/Person.cs
+++ Abschlussprojekt/Abschlussprojekt/Person.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tiefenthaler_Zehetner_Webshop
+namespace Abschlussprojekt
 {
     public class Person //Tiefenthaler
     {
