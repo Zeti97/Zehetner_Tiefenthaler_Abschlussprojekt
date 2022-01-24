5c6047efd84d4c1d6cd9c7a9727bbdbcf8728df2
Markus Zehetner <s2110438016@fhooe.at>
2022-01-24 12:06:46 +0100

    Enum angepasst

--- Abschlussprojekt/Abschlussprojekt/Enums.cs
+++ Abschlussprojekt/Abschlussprojekt/Enums.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Abschlussprojekt
 {
-    class Enums
+    public static class Enums
     {
+         public enum emphasis
+        {
+            Default,
+            Allgemeinbildung,
+            Landwirtschaft_Umwelt,
+            Sport_Gesellschaft,
+            Kultur_Brauchtum,
+            Service_Organisation,
+            Young_International
+        }
     }
 }
