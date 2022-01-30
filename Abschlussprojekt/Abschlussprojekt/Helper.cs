f974c3391a3875a835c3030722077e2c5c4776ad
Patrick Tiefenthaler <patrick.tiefenthaler@students.fh-wels.at>
2022-01-30 16:27:52 +0100

    FilterMethods

---
+++ Abschlussprojekt/Abschlussprojekt/Helper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abschlussprojekt
+{
+    public class Helper
+    {
+        #region methods
+        public List<Person> filteredPerPointsList(List<Person> toFilterPersonList, double pointLimit)
+        {
+            List<Person> filteredPerson = new List<Person>();
+            for (int i = 0; i < toFilterPersonList.ToArray().Length; i++)
+            {
+                if (toFilterPersonList[i].TotalPoints >= pointLimit)
+                {
+                    filteredPerson.Add(toFilterPersonList[i]);
+                }
+            }
+            return filteredPerson;
+        }
+        public List<Person> filteredPerOnTopPerson(List<Person> toFilterPersonList, double pointLimit, string markerYear)
+        {
+            List<Person> filteredPerson = new List<Person>();
+            for (int i = 0; i < toFilterPersonList.ToArray().Length; i++)
+            {
+                for (int j = 0; j < toFilterPersonList[i].OnTopPointsperYear.ToArray().Length; j++)
+                {
+                    if(toFilterPersonList[i].OnTopPointsperYear[j].Marker == markerYear)
+                    {
+                        if(toFilterPersonList[i].OnTopPointsperYear[j].Points >= pointLimit)
+                        {
+                            filteredPerson.Add(toFilterPersonList[i]);
+                        }
+                    }
+                }
+            }
+            return filteredPerson;
+        }
+        #endregion
+    }
+}
